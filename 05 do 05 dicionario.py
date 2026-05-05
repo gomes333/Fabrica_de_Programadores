d1b6a515498088b5151ba46f57080a3a8532b265
gomes333 <isag4659@gmail.com>
2026-05-05 15:21:19 -0300

    Add files via upload

---
+++ 05 do 05 dicionario.py
@@ -0,0 +1,11 @@
+#lista = ["Vanessa","Carla"]
+#dicionario = chave: valor
+pessoa = {"nome": "Vanessa",
+          "idade":32,
+          "cor_cabelo": "marrom"}
+
+
+pessoa["cor_cabelo"] = "Loiro"
+pessoa["idade"] = 40
+pessoa["nome"] = "Carla"
+print(pessoa["cor_cabelo"],pessoa["idade"],pessoa["nome"]) 
